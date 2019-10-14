5aff74127ab6647984b62cbe0c6b06ee1045fc2a
Paul Forstner <forstnerpaul@outlook.de>
2019-10-14 21:55:57 +0200

    [ADD] EventDetail

---
+++ TLoC Teams/ViewControllers/Event/EventDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EventDetailViewController.swift
+//  TLoC Teams
+//
+//  Created by Paul Forstner on 14.10.19.
+//  Copyright © 2019 TLoC. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
